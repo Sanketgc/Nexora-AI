0bdfbca3be55071878b076444a979ac9b609350c
Sanket <sanketchanagond07@gmail.com>
2026-06-28 21:46:49 +0530

    Started With Authentication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,23 @@ import Layout from './pages/Layout'
 import Dashboard from './pages/Dashboard'
 import WriteArticle from './pages/WriteArticle'
 import BlogTitle from './pages/BlogTitle'
+import Generateimage from './pages/Generateimage'
+import Removebackground from './pages/Removebg'
+import RemoveObject from './pages/Removeobject'
+import ReviewResume from './pages/ReviewResume'
+import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
 
 const App =( ) =>{
     return<>
+     <header>
+        <Show when="signed-out">
+          <SignInButton />
+          <SignUpButton />
+        </Show>
+        <Show when="signed-in">
+          <UserButton />
+        </Show>
+      </header>
     <div>
         <Routes>
             <Route path="/" element={<Home />} />
@@ -15,12 +29,14 @@ const App =( ) =>{
                 <Route index element={<Dashboard />} />
                 <Route path="write-article" element={<WriteArticle />} />
                 <Route path="blog-title" element={<BlogTitle />} />
-            </Route>
-
-           
+                <Route path="generate-images" element={<Generateimage />} />
+                <Route path="remove-background" element={<Removebackground />} />
+                <Route path="remove-object" element={<RemoveObject />} />
+                <Route path="review-resume" element={<ReviewResume />} />
+            </Route> 
         </Routes>
     </div>
     </>
 }
 
-export default App6;
+export default App;
