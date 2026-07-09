fc2b58252de6db1bf45c3f1b5ec1c79ebd901b46
Sanket <sanketchanagond07@gmail.com>
2026-07-09 22:20:04 +0530

    Completed With Home Page UI

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import AItools from '../components/AItools'
+import Testimonial from '../components/Testimonial'
+import Plan from '../components/Plan'
+import Footer from '../components/Footer'
 
 const Home =()=>{
     return <>
     <Navbar />
     <Hero />
     <AItools />
+    <Testimonial />
+    <Plan />
+    <Footer />
     </>
 }
 
