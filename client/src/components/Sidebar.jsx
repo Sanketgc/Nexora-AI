4cf85119af9b1ea8786e29dd20f891b4417bdb98
Sanket <sanketchanagond07@gmail.com>
2026-07-12 23:12:42 +0530

    Dashboard Created

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React, { use } from 'react'
-import { useUser, useClerk } from '@clerk/react'
-import { Eraser, FileText, Hash, House, Icon, Image, Scissors, SquarePen, Users } from 'lucide-react'
+import { useUser, useClerk, Show } from '@clerk/react'
+import { Eraser, FileText, Hash, House, Icon, Image, LogOut, Scissors, SquarePen, Users } from 'lucide-react'
 import { NavLink } from 'react-router-dom'
 
 const navitems = [
@@ -31,7 +31,7 @@ bottom-0 ${sidebar ? 'translate-x-0' :
             mx-auto'/>
                 <h1 className='mt-1 text-center'>{user.fullName}</h1>
 
-               <div>
+               <div className='px-6 mt-5 text-sm text-gray-600 font-medium'>
                 {navitems.map (({to, label, Icon})=> (
                     <NavLink key={to} to={to} end={to === '/ai' } 
                     onClick={()=> setsidebar(false)}
@@ -50,6 +50,23 @@ bottom-0 ${sidebar ? 'translate-x-0' :
 
                 ))}
                </div>
+
+            </div>
+
+            <div className='w-full border-t border-gray-200 p-4 px-7 items-center
+            justify-between'>
+                <div onClick={openUserProfile} className='flex gap-2 items-center cursor-pointer'>
+                    <img src={user.imageUrl} className='w-8 rounded-full' alt="" />
+                    <div>
+                        <h1 className='text-sm font-medium'>{user.fullName}</h1>
+                        <p className='text-xs text-gray-500'></p>
+                        <Show when={{plan:'premium'}} fallback="Free">Premium </Show>
+                         Plan
+                    </div>
+                    <LogOut onClick={signOut} className='w-4.5 text-gray-400
+                hover:text-gray-700 transition cursor-pointer'/>
+                </div>
+                
 
             </div>
 
