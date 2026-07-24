866fec975ab8dba167ee6b3dbbf562e552eff9c8
Sanket <sanketchanagond07@gmail.com>
2026-07-24 22:28:40 +0530

    developed generateImage route

--- server/controllers/aicontroller.js
+++ server/controllers/aicontroller.js
@@ -55,3 +55,51 @@ await sql`insert into creations (user_id, prompt, content, type)
         
     }
 }
+
+
+export const generateImage=async(req, res)=>{
+    try{
+        const {userId }=req.auth()
+        const {prompt, length} = req.body
+        const plan = req.plan
+        const free_usage = req.free_usage
+
+        if(plan !== 'Premium' && free_usage >= 10){
+            return res.json({success: false, 
+                message: "Free usage limit reached. Please upgrade to premium plan."})
+        } 
+       
+        const response = await AI.chat.completions.create({
+    model: "gemini-3.5-flash",
+    messages: [
+        {
+            role: "user",
+            content: prompt,
+        },
+    ], 
+    temperature: 0.7,
+    max_tokens: length ,
+});
+
+const content = response.choices[0].message.content
+
+await sql`insert into creations (user_id, prompt, content, type)
+ values (${userId}, ${prompt}, ${content}, 'article')`
+
+ if(plan !== 'Premium'){
+    await clerkClient.users.updateUserMetadata(userId, {
+        privateMetadata: {
+            free_usage: free_usage + 1
+        }
+    })
+ }
+
+ res.json({success: true, content})
+
+
+    } catch(error){
+        res.json({success: false, message: error.message})
+        console.log(error.message);
+        
+    }
+}
--- server/server.js
+++ server/server.js
@@ -1,21 +1,24 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-import { clerkMiddleware} from '@clerk/express'
+import { clerkMiddleware } from '@clerk/express'
 import { auth } from './middlewares/auth.js'
+import { generateArticle, generateImage } from './controllers/aicontroller.js'
 
-
-const app= express()
+const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(clerkMiddleware())
 
-app.get('/', (req, res)=> (res.send("server is live")))
+app.get('/', (req, res) => (res.send("server is live")))
 
+app.use(auth)
+app.post('/api/generate-article', generateArticle)
+app.post('/api/generate-image', generateImage)
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log("app is listening", PORT)
 })
